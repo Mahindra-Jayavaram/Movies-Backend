af1dc2f410f70645468e56dfcb0ab4ede4ae4380
Mahindra Jayavaram <mahindrajayavaram@gmail.com>
2022-06-29 17:29:29 +0530

    CRUD methods implemented

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,8 +1,7 @@
-const { default: mongoose } = require("mongoose");
-const Mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const connect = ()=>{
-    return mongoose.connect("mongodb+srv://mahi:mahi1234@cluster0.bd5ab.mongodb.net/users")
+    return mongoose.connect("mongodb+srv://mahi:mahi123@cluster0.gdsujf1.mongodb.net/movies")
 }
 
 module.exports = connect;
--- src/index.js
+++ src/index.js
@@ -2,24 +2,17 @@ const express = require('express');
 
 const connect = require('./configs/db');
 
-const userController = require('./controllers/user.controller');
+const moviesController = require("./controllers/movies.controller")
 
-const { signup, login } = require('./controllers/auth.controller');
+
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/signup', signup);
-app.post('/login', login);
+app.use("/movies",moviesController);
+app.use("/movies/:id",moviesController);
 
-// app.get("/signup", signup)
-
-// const Signup = require('./src/controllers/user.controller')
-
-// app.use("/signup", signup)
-
-app.use('/users', userController);
 
 app.listen(process.env.PORT || 1342, async () => {
     try{
